9409e08f60d16682648493360d6298e5ed076e7e
Marburg1234 <Marburg1234@users.noreply.github.com>
2024-10-22 11:56:48 +0900

    Ruby FizzBuzz作成

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -1,1 +1,14 @@
-number = 1
+puts "好きな数字を入力してください"
+number = gets.chomp.to_i
+
+if number == 0
+    puts "0では計算を実行できません"
+elsif number % 15 == 0
+    puts "FizzBuzz (5でも3でも割り切れます)"
+elsif number % 5 == 0
+    puts "Fizz (5で割り切れます)"
+elsif number % 3 == 0
+    puts "Buzz (3で割り切れます)"
+else
+    puts "Not FizzBuzz (5と3どちらでも割り切れません)"
+end
