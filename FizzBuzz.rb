2093480d6f18714e14b87c009484cecbe661e271
Marburg1234 <Marburg1234@users.noreply.github.com>
2024-10-24 22:43:24 +0900

    占いif文作成

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -12,3 +12,25 @@ elsif number % 3 == 0
 else
     puts "Not FizzBuzz (5と3どちらでも割り切れません)"
 end
+
+
+#入力した番号で運勢を占う
+puts "------------------"
+puts "占いだよー1-100の好きな番号を入力してください： "
+number = gets.to_i
+
+if number % 21 == 0
+    puts "大吉　大当たり～！"
+elsif number % 6 == 0
+    puts "中吉 まずまず当たり～！"
+elsif number % 4 == 0
+    puts "小吉 まあまあいいことあるよ！"
+elsif number % 3 == 0
+    puts "吉 がんばっていこう！"
+elsif number % 5 == 0
+    puts "凶 おーと悪い予感がする…"
+elsif number % 7 == 0
+    puts "大凶 がーん　お先真っ暗…"
+else
+    puts "占いなんてつゆ知らず"
+end
